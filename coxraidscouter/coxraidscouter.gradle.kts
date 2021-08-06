cf0d04be563af5949a2134844641ca1356b6cdd8
Snake-RS <magnusrnicoll@gmail.com>
2021-08-06 13:04:11 +0100

    re-attempting bootstrap

--- coxraidscouter/coxraidscouter.gradle.kts
+++ coxraidscouter/coxraidscouter.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.openosrsVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.3"
+version = "1.4"
 
 project.extra["PluginName"] = "coxraidscouter" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "Automatically scouts for Chambers of Xeric rotations" // This is the description that is used in the external plugin manager panel
